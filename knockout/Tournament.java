04a67643ffbf2390c2901bb7f7d9bc4b451c1d77
Hyxper <jack.sargeant22@gmail.com>
2022-05-06 11:45:45 +0100

    Got the basics working.... Not sure what the unchecked assignment error is though

--- knockout/Tournament.java
+++ knockout/Tournament.java
@@ -5,44 +5,59 @@ import java.util.Arrays;
 
 public class Tournament {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         double playerPerMatch = 2;
         double playerCount = 8;
-        int noOfRounds = (int) Math.floor(Math.log(playerCount)/Math.log(playerPerMatch));
-                                  
-        ArrayList<Player>[] brackets = new ArrayList[noOfRounds];
+        int noOfRounds = (int) Math.floor(Math.log(playerCount) / Math.log(playerPerMatch));
+
+        ArrayList<Player>[] brackets = new ArrayList<>[noOfRounds];
 
         for (int i = 0; i < noOfRounds; i++) {
-              brackets[i] = new ArrayList<>();
+            brackets[i] = new ArrayList<>();
         }
-                                        
+
 //        System.out.println(Arrays.toString(brackets));
 
-         brackets[0].add(new Player("Andrew"));
-         brackets[0].add(new Player("Drew"));
-         brackets[0].add(new Player("Mic"));
-         brackets[0].add(new Player("Claude"));
-         brackets[0].add(new Player("Jack"));
-         brackets[0].add(new Player("Adam"));
-         brackets[0].add(new Player("Bruce"));
-         brackets[0].add(new Player("Warren"));
+        brackets[0].add(new Player("Andrew"));
+        brackets[0].add(new Player("Drew"));
+        brackets[0].add(new Player("Mic"));
+        brackets[0].add(new Player("Claude"));
+        brackets[0].add(new Player("Jack"));
+        brackets[0].add(new Player("Adam"));
+        brackets[0].add(new Player("Bruce"));
+        brackets[0].add(new Player("Warren"));
 
 
+        int player = 0;
 
-        int x = 0;
-        for (int i = 0; i < playerCount/playerPerMatch; i++) {
-             Match myMatch = new Match(brackets[0].get(x),brackets[0].get(x+1));
-             myMatch.returnWinner();
-             x+=2;
+        outerloop:
+        for (int bracket = 0; bracket < noOfRounds; bracket++) {
+
+            System.out.println("Round "+(bracket+1));
+            System.out.println("Players are: ");
+
+            brackets[bracket].forEach((e) -> System.out.println(e.getName()));
+
+            for (int i = 0; i < playerCount / playerPerMatch; i++) {
+                Match myMatch = new Match(brackets[bracket].get(player), brackets[bracket].get(player + 1));
+                if (brackets[bracket].size()==2){
+                    System.out.println("Winner is..... "+myMatch.returnWinner().getName());
+                    break outerloop;
+                }
+                brackets[bracket+1].add(myMatch.returnWinner());
+                player += 2;
+            }
+
+            player = 0;
+            playerCount = playerCount - brackets[bracket+1].size();
+
         }
-
-
 //         System.out.println(Arrays.toString(brackets));
 //        brackets[0].remove(5);
 //        System.out.println(Arrays.toString(brackets));
 
 //        brackets[0].get(0).getName();
-
+        System.out.println("End");
     }
 }
